da34a2c7f73f395a286ec7c154d1613bf6baeee8
Michal Nalevanko <michal.nalevanko@gmail.com>
2017-03-24 01:33:26 +0000

    Updated the file

--- language_detector/main.py
+++ language_detector/main.py
@@ -4,9 +4,11 @@ from languages import LANGUAGES
 """This is the entry point of the program."""
 
 def get_word_count(text, list_of_words):
+    '''Counts how many words in text appear in list_of_words.'''
     
     count = 0
     text_l = text.split()
+    
     for word in text_l:
         if word in list_of_words:
             count += 1
@@ -22,12 +24,10 @@ def detect_language(text, LANGUAGES):
     for language in LANGUAGES:
         
         result = get_word_count(text, language['common_words'])
-        print(result)
+        #print(result)
         if result > word_count:
             lang = language['name']
             
     return lang
 
-print(detect_language('ich bin du bist', LANGUAGES))
-
-
+#print(detect_language('ich bin du bist sie ist zu', LANGUAGES))
