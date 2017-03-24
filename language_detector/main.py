0585a9f4aaa3feefcb32176e29da00a3955e2c57
Michal Nalevanko <michal.nalevanko@gmail.com>
2017-03-24 01:05:44 +0000

    Our first try

--- language_detector/main.py
+++ language_detector/main.py
@@ -1,8 +1,33 @@
 # -*- coding: utf-8 -*-
+from languages import LANGUAGES
 
 """This is the entry point of the program."""
 
-def detect_language(text, languages):
+def get_word_count(text, list_of_words):
+    
+    count = 0
+    text_l = text.split()
+    for word in text_l:
+        if word in list_of_words:
+            count += 1
+            
+    return count
+
+def detect_language(text, LANGUAGES):
     """Returns the detected language of given text."""
-    # implement your solution here
-    pass
+    lang = None
+    word_count = 0
+    our_test = []
+    
+    for language in LANGUAGES:
+        
+        result = get_word_count(text, language['common_words'])
+        print(result)
+        if result > word_count:
+            lang = language['name']
+            
+    return lang
+
+print(detect_language('ich bin du bist', LANGUAGES))
+
+
